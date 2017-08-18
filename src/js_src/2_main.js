3ccbb74d68aff131437b4e32fea6537fde0341fc
narmatoff <narmatoff@ya.ru>
2017-08-18 19:50:11 +0300

    main_page section & aside started

--- src/js_src/2_main.js
+++ src/js_src/2_main.js
@@ -4,4 +4,15 @@ Pace.on('done', function() {
     $('select').wrap('<div class="selectwrap"></div>');
     $('form').validate();
 
+    var swiper = new Swiper('.slider_wr_core', {
+        pagination: '.slider_wr_core_pagination',
+        paginationClickable: true,
+        // autoplay:8000,
+        // effect: 'flip',
+        paginationBulletRender: function(swiper, index, className) {
+            // return '<span class="header_' + className + index + 1 + '"></span>';
+            return '<span class="slider_wr_core_pagination_bullet ' + className + '"></span>';
+        }
+    });
+
 });
