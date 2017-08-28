7d21639603c83d11d90b8c395259509e28e291f8
narmatoff <narmatoff@ya.ru>
2017-08-28 18:39:24 +0300

    all pages is done(without adaptive)

--- src/js_src/2_main.js
+++ src/js_src/2_main.js
@@ -29,6 +29,52 @@ Pace.on('done', function() {
 
     });
 
-    // $('.parallax-window').parallax({imageSrc: '../img/not_main_hb.jpg'});
+    $('.tabz').responsiveTabs({
+        startCollapsed: 'accordion'
+    });
+
+    $('.popup_img').magnificPopup({
+        type: 'image',
+        callbacks: {
+            open: function() {
+                function getScrollBarWidth() {
+                    var inner = document.createElement('p');
+                    inner.style.width = "100%";
+                    inner.style.height = "200px";
+
+                    var outer = document.createElement('div');
+                    outer.style.position = "absolute";
+                    outer.style.top = "0px";
+                    outer.style.left = "0px";
+                    outer.style.visibility = "hidden";
+                    outer.style.width = "200px";
+                    outer.style.height = "150px";
+                    outer.style.overflow = "hidden";
+                    outer.appendChild(inner);
+                    document.body.appendChild(outer);
+                    var w1 = inner.offsetWidth;
+                    outer.style.overflow = 'scroll';
+                    var w2 = inner.offsetWidth;
+                    if (w1 == w2) w2 = outer.clientWidth;
+
+                    document.body.removeChild(outer);
+
+                    return (w1 - w2);
+                };
+                $('body').css('padding-right', getScrollBarWidth() + "px");
+
+            },
+            close: function() { $('body').css('padding-right', 0); }
+        },
+        gallery: {
+            enabled: true
+        },
+        removalDelay: 300,
+
+        // Class that is added to popup wrapper and background
+        // make it unique to apply your CSS animations just to this exact popup
+        mainClass: 'mfp-fade'
+        // other options
+    });
 
 });
